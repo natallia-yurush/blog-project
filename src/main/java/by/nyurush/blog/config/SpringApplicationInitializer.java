f3fbcafe720431857202afe14c3912f696fc70b8
Natallia Yurush <natallia.yurush@gmail.com>
2021-06-22 01:29:31 +0300

    Create jpa config

---
+++ src/main/java/by/nyurush/blog/config/SpringApplicationInitializer.java
@@ -0,0 +1,27 @@
+package by.nyurush.blog.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //todo congigs
+        return new Class[] {
+                JpaConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+}
+
